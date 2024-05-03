b41f39135797dbfc6da137f8b6b7ea21cc7745a9
galuhays20 <89930078+galuhays20@users.noreply.github.com>
2024-05-03 11:35:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
+from dotenv import load_dotenv
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
 
 app = Flask(__name__)
 
